59df6635a1380c15514bc905e3de7711615864be
Raven Randall <talk.to.raven@proton.me>
2024-05-10 00:00:19 -0500

    tiger's eye ore drops tiger's eye

--- src/main/java/com/mondecitronne/tigerseye/block/BlockTigersEyeOre.java
+++ src/main/java/com/mondecitronne/tigerseye/block/BlockTigersEyeOre.java
@@ -1,8 +1,27 @@
 package com.mondecitronne.tigerseye.block;
 
-import net.minecraft.block.Block;
+import java.util.Random;
+
+import com.mondecitronne.tigerseye.Common;
+
 import net.minecraft.block.BlockOre;
-import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
 
 public class BlockTigersEyeOre extends BlockOre {
+	public BlockTigersEyeOre() {
+		super();
+		setHardness(3.0F);
+		setResistance(5.0F);
+	}
+	
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		return Common.itemTigersEye;
+	}
+	
+	@Override
+	public int quantityDropped(Random random) {
+		return random.nextInt(10) == 1 ? random.nextInt(3) + 1 : 1;
+	}
 }
